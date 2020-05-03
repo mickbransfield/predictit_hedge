3aaa8e0c0481f0d41bc19cc5561e64dd4c3a1df8
mauricebransfield <mauricebransfield@users.noreply.github.com>
2020-05-03 12:53:53 -0400

    Market data by contract and YES/NO price

--- predictit_api.py
+++ predictit_api.py
@@ -6,10 +6,13 @@ import pandas as pd
 # pull in market data from PredictIt's API
 URL = "https://www.predictit.org/api/marketdata/all/"
 response = requests.get(URL)
-jsondata = json.loads(response.text)
+jsondata = response.json()
+
+# find data type (dictionary)
+#print(type(jsondata))
 
 # print individual market to see data available
-print(jsondata['markets'][0])
+print(jsondata['markets'][10])
 
 # pull market IDs
 id = []
@@ -22,7 +25,7 @@ for n in jsondata['markets']:
 	name.append(n.get('name'))
 
 # print contract to see data available
-print(jsondata['markets'][0]['contracts'])
+#print(jsondata['markets'][0]['contracts'])
 
 # Merge IDs and names into one dictionary
 predictit_markets = dict(zip(id, name))
@@ -31,4 +34,23 @@ predictit_markets = dict(zip(id, name))
 predictit_markets = pd.DataFrame.from_dict(predictit_markets, orient='index', columns=['Market Name'])
 predictit_markets['Trump Wins'] = 0
 predictit_markets['Biden Wins'] = 0
-print(predictit_markets)
+#print(predictit_markets)
+
+#Remove null values from data
+def dict_clean(items):
+    result = {}
+    for key, value in items:
+        if value is None:
+            value = 0
+        result[key] = value
+    return result
+
+dict_str = json.dumps(jsondata)
+jsondata = json.loads(dict_str, object_pairs_hook=dict_clean)
+
+# Market data by contract and YES/NO price
+for p in jsondata['markets']:
+    #print('Market ID: ' + '%d' % p['id'],",", 'Market Name: ' + p['name'])
+    for k in p['contracts']:
+        print('Market ID: ' + '%d' % p['id'],",", 'Market Name: ' + p['name'],",",'Contract ID: ' + '%d' % k['id'],",", 'Contract Name: ' + k['name'],",",'Yes Price: ' + '%4.2f' % k['bestBuyYesCost'],",",'No Price: ' + '%4.2f' % k['bestBuyNoCost']) 
+
