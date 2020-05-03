771d9935b535e6927454ff7e2d092dad7ee16841
mauricebransfield <mauricebransfield@users.noreply.github.com>
2020-05-03 14:04:43 -0400

    convert market/contract data to pandas dataframe

--- predictit_api.py
+++ predictit_api.py
@@ -12,7 +12,7 @@ jsondata = response.json()
 #print(type(jsondata))
 
 # print individual market to see data available
-print(jsondata['markets'][10])
+#print(jsondata['markets'][10])
 
 # pull market IDs
 id = []
@@ -28,12 +28,12 @@ for n in jsondata['markets']:
 #print(jsondata['markets'][0]['contracts'])
 
 # Merge IDs and names into one dictionary
-predictit_markets = dict(zip(id, name))
+#predictit_markets = dict(zip(id, name))
 
 # Convert dictionary into dataframe
-predictit_markets = pd.DataFrame.from_dict(predictit_markets, orient='index', columns=['Market Name'])
-predictit_markets['Trump Wins'] = 0
-predictit_markets['Biden Wins'] = 0
+#predictit_markets = pd.DataFrame.from_dict(predictit_markets, orient='index', columns=['Market Name'])
+#predictit_markets['Trump Wins'] = 0
+#predictit_markets['Biden Wins'] = 0
 #print(predictit_markets)
 
 #Remove null values from data
@@ -49,8 +49,20 @@ dict_str = json.dumps(jsondata)
 jsondata = json.loads(dict_str, object_pairs_hook=dict_clean)
 
 # Market data by contract and YES/NO price
-for p in jsondata['markets']:
-    #print('Market ID: ' + '%d' % p['id'],",", 'Market Name: ' + p['name'])
-    for k in p['contracts']:
-        print('Market ID: ' + '%d' % p['id'],",", 'Market Name: ' + p['name'],",",'Contract ID: ' + '%d' % k['id'],",", 'Contract Name: ' + k['name'],",",'Yes Price: ' + '%4.2f' % k['bestBuyYesCost'],",",'No Price: ' + '%4.2f' % k['bestBuyNoCost']) 
+#for p in jsondata['markets']:
+#    #print('Market ID: ' + '%d' % p['id'],",", 'Market Name: ' + p['name'])
+#    for k in p['contracts']:
+#        print('Market ID: ' + '%d' % p['id'],",", 'Market Name: ' + p['name'],",",'Contract ID: ' + '%d' % k['id'],",", 'Contract Name: ' + k['name'],",",'Yes Price: ' + '%4.2f' % k['bestBuyYesCost'],",",'No Price: ' + '%4.2f' % k['bestBuyNoCost']) 
 
+
+# Market data by contract/price in dataframe
+data = []
+for p in jsondata['markets']:
+	for k in p['contracts']:
+		data.append([p['id'],p['name'],k['id'],k['name'],k['bestBuyYesCost'],k['bestBuyNoCost']])
+
+df = pd.DataFrame(data)
+df.columns=['Market ID','Market Name','Contract ID','Contract Name','Yes Price','No Price']
+df['Trump Wins'] = 0
+df['Biden Wins'] = 0
+print(df)
