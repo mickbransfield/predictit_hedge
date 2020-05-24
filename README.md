aad648e45a2f99a3801e77b020dd4e38b83a5ed8
mauricebransfield <mauricebransfield@users.noreply.github.com>
2020-05-24 15:43:14 -0400

    add new file

--- README.md
+++ README.md
@@ -17,12 +17,13 @@ Gain = $1.00 - $0.44 = $0.56
 Fee = $.056 * 10% = $0.056  
 $0.56(gain) - $0.056(fee) - $0.50(Trump loss) = $0.004  
 
-### Three separate files:
+### Four separate files:
 * [predictit_hedge_2020_presidential.py](https://github.com/mauricebransfield/predictit_hedge/blob/master/predictit_hedge_2020_presidential.py), looks for hedge opportunities in Trump and Biden-related contracts.
 * [predictit_hedge.py](https://github.com/mauricebransfield/predictit_hedge/blob/master/predictit_hedge.py), looks for hedge opportunites in user inputed contracts.
-* [predictit_api_write_market_data.py](https://github.com/mauricebransfield/predictit_hedge/blob/master/predictit_api_write_market_data.py), simply writes to CSV the current market and contract prices in PredictIt.
+* [predictit_api_write_market_data.py](https://github.com/mauricebransfield/predictit_hedge/blob/master/predictit_api_write_market_data.py), writes to CSV the current market and contract prices in PredictIt.
+* [predictit_dual_contracts.py](https://github.com/mauricebransfield/predictit_hedge/blob/master/predictit_dual_contracts.py), in markets with only 2 contracts looks for discrepancies in Yes/No prices.
 
 ### Naturally, there are a few qualifications:
 * Similar markets are treated as equals, ie 'Democratic candidate winning presidency' = 'Biden winning presidency.'
 * While this code accounts for the 10% fee on gains from trades, it does not factor in the 5% withdrawl fee.
-* The specific markets/contracts are hard coded to Yes & No prices of 2 contracts for Trump winning and 5 contracts for Biden winning.
+* The 2020 presidential markets/contracts are hard coded to Yes & No prices of 2 contracts for Trump winning and 5 contracts for Biden winning.
---
+++ predictit_dual_contracts.py
@@ -0,0 +1,61 @@
+# Import modules
+import json
+import requests
+import pandas as pd
+pd.options.mode.chained_assignment = None #hide SettingWithCopyWarning
+pd.set_option('display.max_columns', None) # Set it to None to display all columns in the dataframe
+pd.set_option('display.max_colwidth', -1) #  print contents of that column without truncated
+pd.set_option('display.width', None) # Width of the display in characters.
+
+# Pull in market data from PredictIt's API
+URL = "https://www.predictit.org/api/marketdata/all/"
+response = requests.get(URL)
+jsondata = response.json()
+
+# Replace null values with zero
+def dict_clean(items):
+    result = {}
+    for key, value in items:
+        if value is None:
+            value = 0
+        result[key] = value
+    return result
+dict_str = json.dumps(jsondata)
+jsondata = json.loads(dict_str, object_pairs_hook=dict_clean)
+
+# Market data by contract/price in dataframe
+data = []
+for p in jsondata['markets']:
+	for k in p['contracts']:
+		data.append([p['id'],p['name'],k['id'],k['name'],k['bestBuyYesCost'],k['bestBuyNoCost'],k['bestSellYesCost'],k['bestSellNoCost']])
+
+# Pandas dataframe named 'df'
+df = pd.DataFrame(data)
+
+# Update dataframe column names
+df.columns=['Market_ID','Market_Name','Contract_ID','Contract_Name','bestBuyYesCost','bestBuyNoCost','BestSellYesCost','BestSellNoCost']
+
+# Filter dataframe to list of markets with only 2 contracts
+df1 = df.groupby('Market_Name')['Market_ID'].value_counts().reset_index(name ='contract_counts')
+df1 = df1.loc[df1['contract_counts'] == 2]
+Markets_Dual_contracts_list = df1['Market_ID'].tolist()
+Markets_Dual_contracts_df = df[df['Market_ID'].isin(Markets_Dual_contracts_list)]
+
+# Find inefficiencies in Buy Yes and Buy No prices of inversely correlated contracts
+Markets_Dual_contracts_df['interval_AYes_BNo'] = (Markets_Dual_contracts_df.groupby('Market_ID').apply(lambda x: x.bestBuyYesCost-x.bestBuyNoCost.shift(1))).values
+Markets_Dual_contracts_df['interval_ANo_BYes'] = (Markets_Dual_contracts_df.groupby('Market_ID').apply(lambda x: x.bestBuyNoCost-x.bestBuyYesCost.shift(1))).values
+
+# Remove alternating NaN rows
+Markets_Dual_contracts_df = Markets_Dual_contracts_df.dropna(subset=['interval_AYes_BNo'])
+
+# Remove Markets with no delta in Buy Yes and Buy No prices
+Markets_Dual_contracts_df = Markets_Dual_contracts_df.loc[(Markets_Dual_contracts_df['interval_AYes_BNo'] != 0) & (Markets_Dual_contracts_df['interval_ANo_BYes'] != 0)]
+
+# Drop extraneous columns
+Markets_Dual_contracts_df = Markets_Dual_contracts_df.drop(['Contract_ID', 'Contract_Name','bestBuyYesCost','bestBuyNoCost','BestSellYesCost', 'BestSellNoCost'], axis=1)
+
+# Sort by absolute value
+Markets_Dual_contracts_df = Markets_Dual_contracts_df.reindex(Markets_Dual_contracts_df.interval_AYes_BNo.abs().sort_values(ascending=False).index)
+
+print("Below are markets with price discrepancies in two inversely correlated contracts. For example, either a Republican or Democrat presidential candidate will win the state of Idaho, but there might be different buy prices for Republican Yes and Democrat No, and vice versa.\n\n") 
+print(Markets_Dual_contracts_df)
